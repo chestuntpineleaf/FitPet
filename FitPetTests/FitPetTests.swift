0bf0559733b9baa803e87a7c1ea6dc7d2bbfb6c4
lisongye <songyelee@163.com>
2026-05-09 14:19:14 +0800

    Initial Commit

---
+++ FitPetTests/FitPetTests.swift
@@ -0,0 +1,18 @@
+//
+//  FitPetTests.swift
+//  FitPetTests
+//
+//  Created by lisongye on 2026/5/9.
+//
+
+import Testing
+
+struct FitPetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
